0626a552406e5d4853651ce625c6be69d12dc573
Diego de Souza Furlanetto <diegosouzafurlanetto@gmail.com>
2025-06-05 15:58:40 -0300

    TCC

---
+++ Home/script.js
@@ -0,0 +1,5 @@
+var name = prompt('Digite um nome: ');
+var profission = prompt('Digite uma profissao: ');
+
+document.querySelector('.name').innerText = name;
+document.querySelector('.profission').innerText = profission;
