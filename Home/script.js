8678f156ecf12b624347a6a52f753181a80bedca
Diego de Souza Furlanetto <diegosouzafurlanetto@gmail.com>
2025-06-17 09:31:54 -0300

    TCC

--- Home/script.js
+++ Home/script.js
@@ -1,5 +1,18 @@
-var name = prompt('Digite um nome: ');
-var profission = prompt('Digite uma profissao: ');
+function criarcard(){
+    const nome = localStorage.getItem('nome');
+    const ramo = localStorage.getItem('ramo');
 
-document.querySelector('.name').innerText = name;
-document.querySelector('.profission').innerText = profission;
+    let div = document.createElement('div');
+    div.innerHTML = '<div class="card"></div>';
+    let divcard = div.querySelector('.card');
+    divcard.innerHTML = '<div class="imagens"></div> <div class="informacoes"></div>';
+    let divinfor = divcard.querySelector('.informacoes');
+    divinfor.innerHTML = '<div class="foto"></div> <h2 class="name"></h2> <h2 class="profission"></h2> <button class="button">Acessar perfil</button>';
+
+    divinfor.querySelector('.name').innerText = nome;
+    divinfor.querySelector('.profission').innerText = ramo;
+
+    document.querySelector('.homecards').appendChild(divcard);
+}
+
+window.addEventListener('DOMContentLoaded', criarcard);
