b6593e429cbb2178e84a6f5a45b1e74ff921403f
Diego de Souza Furlanetto <diegosouzafurlanetto@gmail.com>
2025-08-06 11:40:19 -0300

    TCC

--- Home/script.js
+++ Home/script.js
@@ -7,7 +7,7 @@ function criarcard(){
     let divcard = div.querySelector('.card');
     divcard.innerHTML = '<div class="imagens"></div> <div class="informacoes"></div>';
     let divinfor = divcard.querySelector('.informacoes');
-    divinfor.innerHTML = '<div class="foto"></div> <h2 class="name"></h2> <h2 class="profission"></h2> <button class="button">Acessar perfil</button>';
+    divinfor.innerHTML = '<div class="foto"></div> <h2 class="name"></h2> <h2 class="profission"></h2> <button class="button">Acessar Perfil</button>';
 
     divinfor.querySelector('.name').innerText = nome;
     divinfor.querySelector('.profission').innerText = ramo;
