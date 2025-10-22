6f34b716a980db1fa1f9cac7c8a9d522f4af57c9
Diego de Souza Furlanetto <diegosouzafurlanetto@gmail.com>
2025-10-22 11:41:05 -0300

    Continuação da tela Favoritos

--- Favoritos/script.js
+++ Favoritos/script.js
@@ -1,5 +1,34 @@
-function selecionar(botao){
-    document.querySelectorAll('.button-tab').forEach(b => b.classList.remove('ativo'));
+const botao_todos = document.getElementById('btn-todos');
+const botao_profi = document.getElementById('btn-profissionais');
+const botao_empre = document.getElementById('btn-empresas');
 
-    botao.classList.add('ativo');
+function filtro_T(){
+    if(botao_todos.classList.contains('ativo') == false){
+        document.querySelector('.button-tab.ativo').classList.remove('ativo');
+        botao_todos.classList.add('ativo');
+        moverHighlight(botao);
+    }
 }
+
+
+function filtro_P(){
+    if(botao_profi.classList.contains('ativo') == false){
+        document.querySelector('.button-tab.ativo').classList.remove('ativo');
+        botao_profi.classList.add('ativo');
+        moverHighlight(botao);
+    }
+}
+
+
+function filtro_E(){
+    if(botao_empre.classList.contains('ativo') == false){
+        document.querySelector('.button-tab.ativo').classList.remove('ativo');
+        botao_empre.classList.add('ativo');
+        moverHighlight(botao);
+    }
+}
+
+
+botao_todos.addEventListener("click", filtro_T);
+botao_profi.addEventListener("click", filtro_P);
+botao_empre.addEventListener("click", filtro_E);
